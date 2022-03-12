8f1d58e8f16da235ab562c07a965cc2fbb9a9d08
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-03-12 13:50:41 +0100

    insert img & header

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,84 @@
+<template>
+    <header>
+        <!-- logo -->
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="logo">
+        </figure>
+
+        <!-- menu -->
+        <div>
+            <ul>
+                <li v-for="(item, index) in menu" :key="index" :class="item.activeMenu ? 'active' : '' ">
+                    {{item.text}}
+                </li>
+            </ul>
+        </div>
+    </header>    
+</template>
+
+<script>
+    export default{
+        name: 'MainHeader',
+        data (){
+            return{
+                menu: [
+                    {
+                        text: 'Characters',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'Comics',
+                        link: '#',
+                        activeMenu: true
+                    },
+                    {
+                        text: 'Movies',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'TV',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'Games',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'Collectibles',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'Videos',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'Fans',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'News',
+                        link: '#',
+                        activeMenu: false
+                    },
+                    {
+                        text: 'Shop',
+                        link: '#',
+                        activeMenu: false
+                    }
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
